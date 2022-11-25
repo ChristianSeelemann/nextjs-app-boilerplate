81a1ac242f4705deb9c16afb73d296cf1e8be26b
ChristianSeelemann <chrisstiff@gmx.de>
2022-11-25 13:02:51 +0100

    ➕ Install Next.js 13 with Typescript

---
+++ app/page.tsx
@@ -0,0 +1,21 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1 className={styles.title}>NEXT.JS 13 BOILERPLATE</h1>
+
+        <p className={styles.description}>This Boilerplate contains:</p>
+
+        <div className={styles.grid}>
+          <p className={styles.card}>Next.js 13</p>
+
+          <p className={styles.card}>Next.js 13</p>
+
+          <p className={styles.card}>Next.js 13</p>
+        </div>
+      </main>
+    </div>
+  );
+}
