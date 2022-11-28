59d9166823a4c3a24ba007269ea8be66e06b9cd8
ChristianSeelemann <chrisstiff@gmx.de>
2022-11-28 19:22:56 +0100

    👽️ Add get user api route

---
+++ pages/api/user/get.ts
@@ -0,0 +1,41 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import connectMongo from "../../../app/lib/dbConnectionAPI";
+import User from "../../../app/models/userModel";
+import Session from "../../../app/models/sessionModel";
+
+type Data = {
+  user: {
+    _id: string;
+    name: string;
+    email: string;
+    image: string;
+    emailVerified: boolean;
+    banned: boolean | null;
+  };
+  token: string | string[] | undefined;
+} | null;
+
+export default async function getUser(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  await connectMongo();
+
+  const session = await Session.findOne({
+    sessionToken: req.query.token,
+  });
+
+  if (session) {
+    const user = await User.findById(session.userId);
+
+    const today = new Date();
+    await Session.findOneAndUpdate(
+      { sessionToken: req.query.token },
+      { expires: today.setDate(today.getDate() + 30) }
+    );
+    res.status(200).json({ user: user, token: req.query.token });
+  } else {
+    const user = null;
+    res.status(401).json(user);
+  }
+}
