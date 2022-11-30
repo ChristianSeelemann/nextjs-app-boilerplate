9911ca61d6c922b8ae4876db74fb081c891e6dd1
ChristianSeelemann <chrisstiff@gmx.de>
2022-11-30 19:39:49 +0100

    👽️ Add update user API rout

---
+++ pages/api/user/update.ts
@@ -0,0 +1,33 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import connectMongo from "../../../app/lib/dbConnectionAPI";
+import User from "../../../app/models/userModel";
+import Session from "../../../app/models/sessionModel";
+import apiWrapper from "../../../app/lib/apiWrapper";
+
+type Data = {
+  user: {
+    _id: string;
+    name: string;
+    email: string | null;
+    image: string;
+    emailVerified: boolean;
+    banned: boolean | null;
+    nickname: string | null;
+    colormode: string | null;
+  };
+  token: string | string[] | undefined;
+} | null;
+
+export default async function getUser(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  await connectMongo();
+
+  apiWrapper(req, res, async (session: any) => {
+    await User.findByIdAndUpdate(session.userId, {
+      colormode: req.body.colormode,
+    });
+    res.status(200).json({ user: req.body, token: req.query.token });
+  });
+}
