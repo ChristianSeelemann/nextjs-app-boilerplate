9bd4f5322a30cf112d5bd312338d78ffea819e72
ChristianSeelemann <chrisstiff@gmx.de>
2022-11-30 19:40:17 +0100

    👽️ Use API wrapper for user get route

--- pages/api/user/get.ts
+++ pages/api/user/get.ts
@@ -2,6 +2,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import connectMongo from "../../../app/lib/dbConnectionAPI";
 import User from "../../../app/models/userModel";
 import Session from "../../../app/models/sessionModel";
+import apiWrapper from "../../../app/lib/apiWrapper";
 
 type Data = {
   user: {
@@ -12,6 +13,7 @@ type Data = {
     emailVerified: boolean;
     banned: boolean | null;
     nickname: string | null;
+    colormode: string | null;
   };
   token: string | string[] | undefined;
 } | null;
@@ -22,34 +24,14 @@ export default async function getUser(
 ) {
   await connectMongo();
 
-  // Check if token is present
-  if (req.query.token) {
-    const session = await Session.findOne({
-      sessionToken: req.query.token,
-    });
-    // Check if API Key is valid
-    if (req.query.api_key == process.env.API_KEY) {
-      // Check if session is valid
-      if (session) {
-        const user = await User.findById(session.userId);
+  apiWrapper(req, res, async (session: any) => {
+    const user = await User.findById(session.userId);
 
-        const today = new Date();
-        await Session.findOneAndUpdate(
-          { sessionToken: req.query.token },
-          { expires: today.setDate(today.getDate() + 30) }
-        );
-        res.status(200).json({ user: user, token: req.query.token });
-        // Session is invalid
-      } else {
-        const user = null;
-        res.status(401).json(user);
-      }
-      // API Key is invalid
-    } else {
-      res.status(401).json(null);
-    }
-    //Missing token
-  } else {
-    res.status(401).json(null);
-  }
+    const today = new Date();
+    await Session.findOneAndUpdate(
+      { sessionToken: req.query.token },
+      { expires: today.setDate(today.getDate() + 30) }
+    );
+    res.status(200).json({ user: user, token: req.query.token });
+  });
 }
