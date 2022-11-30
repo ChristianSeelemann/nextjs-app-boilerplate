f3a907ac2b4fa23f2bb2c81f4f9e8af38f026c86
ChristianSeelemann <chrisstiff@gmx.de>
2022-11-30 22:26:57 +0100

    🎨 Finish the color mode feature

--- app/components/colormode/colorModeToggle.tsx
+++ app/components/colormode/colorModeToggle.tsx
@@ -5,21 +5,31 @@ import { MdDarkMode } from "react-icons/md";
 import { HiSun } from "react-icons/hi";
 import { User } from "../../../types/user";
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 function getColorMode({ user }: { user: User }) {
   const colorModeCookie = getCookie("next-colormode");
   const defaultColorMode = process.env.NEXT_PUBLIC_DEFAULT_COLORMODE;
 
   let actualColorMode;
+  // Check if user is logged in
   if (!user) {
+    // No? Check if cookie is set
     if (!colorModeCookie) {
       actualColorMode = defaultColorMode;
     } else {
       actualColorMode = colorModeCookie;
     }
   } else {
+    // Yes? Check if user has a color mode set
     if (!user.user.colormode) {
-      actualColorMode = defaultColorMode;
+      // No? Check if cookie is set
+      if (!colorModeCookie) {
+        actualColorMode = defaultColorMode;
+      } else {
+        actualColorMode = colorModeCookie;
+      }
+      // Yes? Set actualColorMode to user's color mode
     } else {
       actualColorMode = user.user.colormode;
     }
@@ -30,61 +40,49 @@ function getColorMode({ user }: { user: User }) {
 async function toggleColorMode({ user }: { user: User }) {
   const actualColor = getColorMode({ user });
 
-  if (actualColor === "dark") {
-    setCookie("next-colormode", "light");
-    if (user) {
-      await fetch(
-        process.env.NEXT_PUBLIC_BASE_URL +
-          "/api/user/update?token=" +
-          user.token +
-          "&api_key=" +
-          process.env.NEXT_PUBLIC_API_KEY,
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            colormode: "light",
-          }),
-        }
-      );
-    }
-  } else {
-    setCookie("next-colormode", "dark");
-    if (user) {
-      await fetch(
-        process.env.NEXT_PUBLIC_BASE_URL +
-          "/api/user/update?token=" +
-          user.token +
-          "&api_key=" +
-          process.env.NEXT_PUBLIC_API_KEY,
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            colormode: "dark",
-          }),
-        }
-      );
-    }
+  // Everytime set cookie
+  setCookie("next-colormode", actualColor === "light" ? "dark" : "light");
+  // If user is logged in, set user's color mode
+  if (user) {
+    await fetch(
+      process.env.NEXT_PUBLIC_BASE_URL +
+        "/api/user/update?token=" +
+        user.token +
+        "&api_key=" +
+        process.env.NEXT_PUBLIC_API_KEY,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          colormode: actualColor === "light" ? "dark" : "light",
+        }),
+      }
+    );
   }
 }
 
 export default function ColorModeToggle({ user }: { user: User }) {
   const router = useRouter();
+  const [colorMode, setColorMode] = useState<any>();
+
+  // Set color mode on load to avoid hydration mismatch
+  useEffect(() => {
+    setColorMode(getColorMode({ user }));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <div>
       <button
         onClick={() => {
           toggleColorMode({ user });
+          setColorMode(colorMode === "dark" ? "light" : "dark");
           router.refresh();
         }}
       >
-        {getColorMode({ user }) === "dark" ? <HiSun /> : <MdDarkMode />}
+        {colorMode ? colorMode === "dark" ? <HiSun /> : <MdDarkMode /> : null}
       </button>
     </div>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
-import { cookies } from "next/headers";
 import "./globals.css";
 import Header from "./components/parts/Header";
 import Footer from "./components/parts/Footer";
 import getUser from "./components/authentification/getUser";
+import getColorMode from "./lib/getColorMode";
 
 getUser();
 
@@ -14,26 +14,7 @@ export default async function RootLayout({
   const userData = await getUser();
   const user = userData?.user;
 
-  const allCookies = cookies();
-  const colormodeCookie = allCookies.get("next-colormode");
-
-  // Choose the color mode based on the user's preference
-  let colorMode;
-
-  if (!user || !user.colormode) {
-    // If the user is not logged in, use the colormode from the cookie
-    if (!colormodeCookie) {
-      colorMode = process.env.NEXT_PUBLIC_DEFAULT_COLORMODE;
-    } else {
-      colorMode = colormodeCookie.value;
-    }
-    // If the user is logged in, use the colormode from the database
-  } else if (user && user.colormode) {
-    colorMode = user.colormode;
-    // User not logged in and no cookie set
-  } else {
-    colorMode = process.env.NEXT_PUBLIC_DEFAULT_COLORMODE;
-  }
+  const colorMode = getColorMode({ user });
 
   return (
     <html lang="en" className={colorMode}>
