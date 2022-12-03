159eb30e5d59b0cb31cd95e1d7e9774cdc07207e
ChristianSeelemann <chrisstiff@gmx.de>
2022-12-03 21:11:37 +0100

    ♻️ Refactor getUser Helpter

--- app/lib/getUser.ts
+++ app/lib/getUser.ts
@@ -6,9 +6,10 @@ export default async function getUser() {
 
   //Only read user data when token cookie is present
   if (token) {
+    // Try to get user data from the token
     const getUser = await fetch(
       process.env.BASE_URL +
-        "/api/user/get?token=" +
+        "/api/user/getSelf?token=" +
         token.value +
         "&api_key=" +
         process.env.API_KEY,
@@ -20,14 +21,15 @@ export default async function getUser() {
       }
     );
     const user = getUser.json();
-
-    //No user found
+    //When no user data is found, return null
     if (!user) {
       return null;
+      // When user data is found, return user data
     } else {
       return user;
     }
+    // When no token cookie found set user to null
+  } else {
+    return null;
   }
-
-  return null;
 }
