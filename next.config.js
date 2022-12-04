12c867c1d605be3eb62950bea2d8e5e2f6b3c63d
ChristianSeelemann <chrisstiff@gmx.de>
2022-12-04 23:14:48 +0100

    🔧 Add discord to image servers

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  images: {
+    domains: ["cdn.discordapp.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
