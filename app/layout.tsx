added82f30db91b78ca2a1f66a7bbfe5eeb5c7c7
ChristianSeelemann <chrisstiff@gmx.de>
2022-12-06 00:57:18 +0100

    ♻️ Add MUI fonts and lastOnline helper to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,13 +4,23 @@ import Footer from "./components/parts/Footer";
 import getUser from "./lib/getUser";
 import getColorMode from "./lib/getColorMode";
 
+// Fonts for Material UI
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import lastOnline from "./lib/lastOnline";
+
 export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  // Get user data
   const userData = await getUser();
-
+  // Set lastOnline to User
+  lastOnline();
+  // Get color mode
   const colorMode = getColorMode(userData);
 
   return (
