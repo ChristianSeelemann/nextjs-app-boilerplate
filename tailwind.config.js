c854e5c01fa4649bfbee46f5f8ff999e348b97d8
ChristianSeelemann <chrisstiff@gmx.de>
2022-12-14 21:31:22 +0100

    🔧 Add colors to Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require("tailwindcss/colors");
+
 module.exports = {
+  mode: "jit",
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        bglight: "#F8F8F8",
+        bgdark: "#111872",
+        primary: colors.blue,
+        light: colors.slate,
+        dark: colors.neutral,
+      },
+    },
   },
   plugins: [],
   darkMode: "class",
