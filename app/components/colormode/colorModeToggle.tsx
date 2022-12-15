eae74c0ae9b3868f5842c29638c2a150ae0bcda8
ChristianSeelemann <chrisstiff@gmx.de>
2022-12-15 03:55:26 +0100

    ♻️ Refactor colorModeToggle component

--- app/components/colormode/colorModeToggle.tsx
+++ app/components/colormode/colorModeToggle.tsx
@@ -6,71 +6,72 @@ import { HiSun } from "react-icons/hi";
 import { Session } from "../../../types/auth";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-import { IconButton } from "@mui/material";
-import Tooltip from "../ui/tooltip";
+import { ButtonGroup, MenuItem } from "@mui/material";
+import Button from "../ui/button";
 
-function getColorMode({ session }: { session: Session }) {
-  const colorModeCookie = getCookie("next-colormode");
-  const defaultColorMode = process.env.NEXT_PUBLIC_DEFAULT_COLORMODE;
+export default function ColorModeToggle({ session }: { session: Session }) {
+  function getColorMode({ session }: { session: Session }) {
+    const colorModeCookie = getCookie("next-colormode");
+    const defaultColorMode = process.env.NEXT_PUBLIC_DEFAULT_COLORMODE;
 
-  let actualColorMode;
+    let actualColorMode;
 
-  // Check if user is logged in
-  if (session) {
-    // Check if user has a color mode set
-    if (session.user?.colormode) {
-      actualColorMode = session.user.colormode;
-      // If not, check if there is a cookie
+    // Check if user is logged in
+    if (session) {
+      // Check if user has a color mode set
+      if (session.user?.colormode) {
+        actualColorMode = session.user.colormode;
+        // If not, check if there is a cookie
+      } else {
+        // If there is a cookie, use it
+        if (colorModeCookie) {
+          actualColorMode = colorModeCookie;
+          // If not, use the default color mode
+        } else {
+          actualColorMode = defaultColorMode;
+        }
+      }
     } else {
-      // If there is a cookie, use it
+      // If user is not logged in, check cookie
       if (colorModeCookie) {
         actualColorMode = colorModeCookie;
-        // If not, use the default color mode
+        // If cookie is not set, set default color mode
       } else {
         actualColorMode = defaultColorMode;
       }
     }
-  } else {
-    // If user is not logged in, check cookie
-    if (colorModeCookie) {
-      actualColorMode = colorModeCookie;
-      // If cookie is not set, set default color mode
-    } else {
-      actualColorMode = defaultColorMode;
+    // Return the actual color mode
+    return actualColorMode;
+  }
+
+  async function toggleColorMode({ session }: { session: Session }) {
+    const actualColor = getColorMode({ session });
+
+    // Everytime set cookie
+    setCookie("next-colormode", actualColor === "light" ? "dark" : "light");
+    // If user is logged in, set user's color mode
+    if (session) {
+      await fetch(
+        process.env.NEXT_PUBLIC_BASE_URL +
+          "/api/user/updateSelf?token=" +
+          session.token +
+          "&api_key=" +
+          process.env.NEXT_PUBLIC_API_KEY,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            colormode: actualColor === "light" ? "dark" : "light",
+          }),
+        }
+      );
     }
   }
-  // Return the actual color mode
-  return actualColorMode;
-}
 
-async function toggleColorMode({ session }: { session: Session }) {
-  const actualColor = getColorMode({ session });
-
-  // Everytime set cookie
-  setCookie("next-colormode", actualColor === "light" ? "dark" : "light");
-  // If user is logged in, set user's color mode
-  if (session) {
-    await fetch(
-      process.env.NEXT_PUBLIC_BASE_URL +
-        "/api/user/updateSelf?token=" +
-        session.token +
-        "&api_key=" +
-        process.env.NEXT_PUBLIC_API_KEY,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          colormode: actualColor === "light" ? "dark" : "light",
-        }),
-      }
-    );
-  }
-}
-
-export default function ColorModeToggle({ session }: { session: Session }) {
   const router = useRouter();
+
   const [colorMode, setColorMode] = useState<any>();
 
   // Set color mode on load to avoid hydration mismatch
@@ -82,37 +83,54 @@ export default function ColorModeToggle({ session }: { session: Session }) {
   return (
     <>
       {colorMode && (
-        <Tooltip
-          title={colorMode === "light" ? "Set Dark Mode" : "Set Light Mode"}
-        >
-          <IconButton
-            className={
-              colorMode === "dark"
-                ? "bg-dark-800 rounded-md hover:bg-dark-700"
-                : "bg-light-200 rounded-md hover:bg-light-300"
-            }
-            aria-label="Change Colormode"
-            color="inherit"
-            sx={{
-              "&.MuiIconButton-root.Mui-focusVisible": {
-                borderRadius: "8px",
-              },
-            }}
-            onClick={() => {
-              toggleColorMode({ session });
-              setColorMode(colorMode === "dark" ? "light" : "dark");
-              setTimeout(() => {
-                router.refresh();
-              }, 250);
-            }}
+        <MenuItem className="menuitem hover:!bg-transparent !py-0">
+          <ButtonGroup
+            variant="contained"
+            aria-label="Change Color Mode"
+            className="mt-2 mb-1"
           >
-            {colorMode === "dark" ? (
-              <HiSun className="text-yellow-500" />
-            ) : (
-              <MdDarkMode className="text-light-900" />
-            )}
-          </IconButton>
-        </Tooltip>
+            <Button
+              text="Light"
+              variant="outlined"
+              startIcon={<HiSun className="dark:text-yellow-500" />}
+              ariaLabel="Choose Light Mode"
+              disabled={colorMode === "light" ? true : false}
+              classes={
+                colorMode === "light"
+                  ? "defaultbutton font-chakrabold !bg-light-200 !border-light-800"
+                  : "defaultbutton font-chakrabold !border-dark-100 hover:!bg-dark-600 hover:!text-dark-50"
+              }
+              onClick={() => {
+                toggleColorMode({ session });
+                setColorMode("light");
+                setTimeout(() => {
+                  router.refresh();
+                }, 250);
+              }}
+            />
+            <Button
+              text="Dark"
+              variant="outlined"
+              startIcon={
+                <MdDarkMode className="text-light-900 dark:text-dark-300" />
+              }
+              ariaLabel="Choose Dark Mode"
+              disabled={colorMode === "dark" ? true : false}
+              classes={
+                colorMode === "dark"
+                  ? "defaultbutton font-chakrabold !text-dark-300 !bg-dark-700"
+                  : "defaultbutton font-chakrabold !bg-transparent !text-light-800 !border-light-800 hover:!bg-light-300"
+              }
+              onClick={() => {
+                toggleColorMode({ session });
+                setColorMode("dark");
+                setTimeout(() => {
+                  router.refresh();
+                }, 250);
+              }}
+            />
+          </ButtonGroup>
+        </MenuItem>
       )}
     </>
   );
